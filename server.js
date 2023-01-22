171ca744fde5bc84c438422588722c8c5ef8789c
Mouad <mouadkouibaa8@gmail.com>
2023-01-23 00:46:39 +0100

    server.js modified

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./Data/Notes.json');
-const middlewares = jsonServer.defaults({
-    static: './build'
-});
-const PORT = process.env.PORT || 8000;
+const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 3000;
 server.use(middlewares);
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-}))
+server.use(router)
 server.use(router);
 server.listen(PORT, () => {
     console.log('Server is running');
